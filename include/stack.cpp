8d0101d18baf03044d240bec8d0fc033b7d725d2
TAHK6e3TOPMO3OB <kirill_stefanenkov@rambler.ru>
2016-09-19 15:28:22 +0400

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -16,11 +16,17 @@ public:
 	T pop();
 	stack& operator=(const stack&);
 private:
-	T* copy_mas(const T *, size_t, size_t);
 	T * array_;
 	size_t array_size_;
 	size_t count_;
 };
+
+template<typename T>
+T* copy_mas(const T *p1, size_t c, size_t s){
+	T *p2 = new T[s];
+	copy(p1, p1 + c, p2);
+	return p2;
+}
 
 template <typename T>
 stack<T>::stack() : array_(nullptr), array_size_(0), count_(0){};
@@ -41,21 +47,22 @@ size_t stack<T>::count()const { return count_; }
 template <typename T>
 void stack<T>::push(T const &a){
 	if (count_ == array_size_){
-		T *p = array_;
-		array_ = copy_mas(p, count_, array_size_ * 2+(count_==0));
-		if(p!=nullptr) delete[]p;
 		array_size_ =array_size_*2+(count_==0);
+		T *p = copy_mas(array_, count_, array_size_);
+		delete [] array_;
+		array_=p;
 	}
 	array_[count_] = a;
-	count_++;
+	++count_;
 }
 
 template <typename T>
 T stack<T>::pop(){
 	if (count_> 0) {
-		count_--;
+		--count_;
 		return array_[count_];
 	}
+	throw"";
 }
 
 template<typename T>
@@ -67,13 +74,6 @@ stack<T>& stack<T>::operator=(const stack& b){
 		array_ = copy_mas(b.array_, count_, array_size_);
 	}
 	return *this;
-}
-
-template<typename T>
-T* stack<T>::copy_mas(const T *p1, size_t c, size_t s){
-	T *p2 = new T[s];
-	copy(p1, p1 + c, p2);
-	return p2;
 }
 
 #endif

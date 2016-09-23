8b72bcc3c4e6d8d5b6cdedd520c55de216419fc0
TAHK6e3TOPMO3OB <kirill_stefanenkov@rambler.ru>
2016-09-23 19:09:59 +0300

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -8,13 +8,15 @@ template <typename T>
 class stack
 {
 public:
-	stack();
-	stack(const stack&);
-	~stack();
-	size_t count() const;
-	void push(T const &);
-	T pop();
-	stack& operator=(const stack&);
+	stack(); /* noexcept */
+	stack(const stack&); /* strong */
+	~stack(); /* noexcept */
+	size_t count() const; /* noexcept */
+	void push(T const &); /* strong */
+	const T& top(); /* strong */
+	void pop(); /* strong */
+	stack& operator=(const stack&); /* strong */
+	bool empty(); /* noexcept */
 private:
 	T * array_;
 	size_t array_size_;
@@ -23,18 +25,22 @@ private:
 
 template<typename T>
 T* copy_mas(const T *p1, size_t c, size_t s){
-	T *p2 = new T[s];
-	copy(p1, p1 + c, p2);
-	return p2;
+	if (s >= c){
+		T *p2 = new T[s];
+		try{ copy(p1, p1 + c, p2); }
+		catch (...){ delete[] p2; throw("error in copy"); }
+		return p2;
+	}
+	else throw("size < count");
 }
 
 template <typename T>
 stack<T>::stack() : array_(nullptr), array_size_(0), count_(0){};
 
 template <typename T>
-stack<T>::stack(const stack& x) : array_size_(x.array_size_), count_(x.count_){
-	array_ = copy_mas(x.array_, count_, array_size_);
-}
+stack<T>::stack(const stack& x) : array_size_(x.array_size_),
+	count_(x.count_),
+	array_(copy_mas(x.array_, x.count_, x.array_size_)){};
 
 template <typename T>
 stack<T>::~stack(){
@@ -47,33 +53,40 @@ size_t stack<T>::count()const { return count_; }
 template <typename T>
 void stack<T>::push(T const &a){
 	if (count_ == array_size_){
-		array_size_ =array_size_*2+(count_==0);
-		T *p = copy_mas(array_, count_, array_size_);
-		delete [] array_;
-		array_=p;
+		T *p = copy_mas(array_, count_, array_size_ * 2 + (count_ == 0));
+		delete[] array_;
+		array_ = p;
+		array_size_ = array_size_ * 2 + (count_ == 0);
 	}
 	array_[count_] = a;
 	++count_;
 }
 
 template <typename T>
-T stack<T>::pop(){
-	if (count_> 0) {
-		--count_;
-		return array_[count_];
-	}
-	throw("stack pust");
+const T& stack<T>::top(){
+	if (count_ > 0) return array_[--count_];
+	else throw("stack pust");
+}
+
+template <typename T>
+void stack<T>::pop(){
+	if (count_> 0) --count_;
+	else throw("stack pust");
 }
 
 template<typename T>
 stack<T>& stack<T>::operator=(const stack& b){
 	if (this != &b){
-		delete[] array_;
+		T *p = array_;
+		array_ = copy_mas(b.array_, b.count_, b.array_size_);
+		delete[] p;
 		count_ = b.count_;
 		array_size_ = b.array_size_;
-		array_ = copy_mas(b.array_, count_, array_size_);
 	}
 	return *this;
 }
+
+template<typename T>
+bool stack<T>::empty(){ return(count_ == 0); }
 
 #endif

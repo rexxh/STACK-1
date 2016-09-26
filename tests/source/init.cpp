64453158234b57dee0f4a7191b2015691dcf342d
TAHK6e3TOPMO3OB <kirill_stefanenkov@rambler.ru>
2016-09-26 10:20:22 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -13,7 +13,7 @@ SCENARIO("push", "[push]"){
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
-  REQUIRE(s.top()==1);
+  REQUIRE(s.pop()==1);
 }
 
 SCENARIO("pop", "[pop]"){
@@ -28,7 +28,7 @@ SCENARIO("prisv", "[prisv]"){
   stack<int> s2;
   s2=s;
   REQUIRE(s.count()==1);
-  REQUIRE(s.top()==1);
+  REQUIRE(s.pop()==1);
 }
 
 SCENARIO("cop", "[cop]"){
@@ -36,10 +36,10 @@ SCENARIO("cop", "[cop]"){
   s.push(1);
   stack<int> s2=s;
   REQUIRE(s2.count()==1);
-  REQUIRE(s2.top()==1);
+  REQUIRE(s2.pop()==1);
 }
 
-SCENARIO("top", "[top]"){
+/*SCENARIO("top", "[top]"){
   stack<int> s;
   s.push(1);
   REQUIRE(s.top()==1);
@@ -50,5 +50,5 @@ SCENARIO("empty", "[empty]"){
   s1.push(1);
   REQUIRE(!s1.empty());
   REQUIRE(s2.empty());
-}
+}*/
 

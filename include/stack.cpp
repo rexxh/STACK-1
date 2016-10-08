e879d70591bfb1752dae14cad3c064ce2b66f7ea
TAHK6e3TOPMO3OB <kirill_stefanenkov@rambler.ru>
2016-10-08 15:22:51 +0300

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -18,7 +18,7 @@ protected:
 };
 
 template <typename T>
-allocator<T>::allocator(size_t size = 0) : ptr_((T*)(operator new(size*sizeof(T)))), size_(size), count_(0){};
+allocator<T>::allocator(size_t size) : ptr_((T*)(operator new(size*sizeof(T)))), size_(size), count_(0){};
 
 template<typename T>
 allocator<T>::~allocator(){ delete ptr_; }
@@ -58,53 +58,53 @@ stack<T>::stack() : allocator<T>(){};
 
 template <typename T>
 stack<T>::stack(const stack& x) : allocator<T>(x.size_){
-	ptr_ = copy_mas(x.ptr_, x.count_, x.size_);
-	count_ = x.count_;
+	allocator<T>::ptr_ = copy_mas(x.allocator<T>::ptr_, x.allocator<T>::count_, x.allocator<T>::size_);
+	allocator<T>::count_ = x.allocator<T>::count_;
 };
 
 template <typename T>
 stack<T>::~stack(){}
 
 template <typename T>
-size_t stack<T>::count()const { return count_; }
+size_t stack<T>::count()const { return allocator<T>::count_; }
 
 template <typename T>
 void stack<T>::push(T const &a){
-	if (count_ == size_){
-		T *p = copy_mas(ptr_, count_, size_ * 2 + (count_ == 0));
+	if (allocator<T>::count_ == allocator<T>::size_){
+		T *p = copy_mas(allocator<T>::ptr_, allocator<T>::count_, allocator<T>::size_ * 2 + (allocator<T>::count_ == 0));
 		delete ptr_;
-		ptr_ = p;
-		size_ = size_ * 2 + (count_ == 0);
+		allocator<T>::ptr_ = p;
+		allocator<T>::size_ = allocator<T>::size_ * 2 + (count_ == 0);
 	}
-	ptr_[count_] = a;
-	++count_;
+	allocator<T>::ptr_[allocator<T>::count_] = a;
+	++allocator<T>::count_;
 }
 
 template <typename T>
 const T& stack<T>::top(){
-	if (count_ > 0) return ptr_[count_ - 1];
+	if (allocator<T>::count_ > 0) return allocator<T>::ptr_[allocator<T>::count_ - 1];
 	else throw("stack pust");
 }
 
 template <typename T>
 void stack<T>::pop(){
-	if (count_> 0) --count_;
+	if (allocator<T>::count_> 0) --allocator<T>::count_;
 	else throw("stack pust");
 }
 
 template<typename T>
 stack<T>& stack<T>::operator=(const stack& b){
 	if (this != &b){
-		T *p = ptr_;
-		ptr_ = copy_mas(b.ptr_, b.count_, b.size_);
+		T *p = allocator<T>::ptr_;
+		allocator<T>::ptr_ = copy_mas(b.allocator<T>::ptr_, b.allocator<T>::count_, b.allocator<T>::size_);
 		delete p;
-		count_ = b.count_;
-		size_ = b.size_;
+		allocator<T>::count_ = b.allocator<T>::count_;
+		allocator<T>::size_ = b.allocator<T>::size_;
 	}
 	return *this;
 }
 
 template<typename T>
-bool stack<T>::empty(){ return(count_ == 0); }
+bool stack<T>::empty(){ return(allocator<T>::count_ == 0); }
 
 #endif

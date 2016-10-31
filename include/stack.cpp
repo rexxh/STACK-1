1861ef5763265f080c2d57fbe8f7c0ba5c656000
TAHK6e3TOPMO3OB <kirill_stefanenkov@rambler.ru>
2016-10-31 17:21:17 +0400

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -123,7 +123,7 @@ auto allocator<T>::empty() const -> bool { return (map_->counter() == 0); }
 
 template<typename T>
 auto allocator<T>::destroy(T * first, T * last)->void{
-	if(first>=prt_&&last<=ptr_+this->count())for (; first != last; ++first) {
+	if(first>=ptr_&&last<=ptr_+this->count())for (; first != last; ++first) {
 		destroy(&*first);
 	}
 }
@@ -164,7 +164,7 @@ stack<T>::stack(size_t size) : allocator_(size){}
 template<typename T>
 auto stack<T>::operator =(stack const & other)-> stack &{ 
 	if (this != &other) {
-		(allocator(other.allocator_)).swap(allocator_);
+		(allocator<T>(other.allocator_)).swap(allocator_);
 	}
 	return *this;
 }
